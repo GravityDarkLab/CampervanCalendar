7f685c66ac6c5b8faff69cf3e93d5425cdc53eab
Achraf Labidi <labidiachraf6@gmail.com>
2024-04-09 13:22:07 +0200

    Improve responsivness

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,35 +1,32 @@
 <template>
-  <div id="calendar">
-    <div class="controls">
-      <div class="flex justify-center items-center m-5">
-        <div class="relative">
-          <select
-            v-model="selectedStation"
-            @change="getStationBookings"
-            class="block px-3 py-2 bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent rounded-md"
-          >
-            <option v-for="station in stations" :key="station.id" :value="station.id">
-              {{ station.name }}
-            </option>
-          </select>
-          <div
-            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-          >
-            <svg
-              class="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              />
-            </svg>
-          </div>
+  <div>
+    <div class="flex justify-center items-center m-5 space-x-4">
+      <div class="relative">
+        <select
+          v-model="selectedStation"
+          @change="getStationBookings"
+          class="block px-3 py-2 bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent rounded-md"
+        >
+          <option v-for="station in stations" :key="station.id" :value="station.id">
+            {{ station.name }}
+          </option>
+        </select>
+        <div
+          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500"
+        >
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            />
+          </svg>
         </div>
       </div>
     </div>
+  </div>
+  <div id="calendar">
     <div class="calendar-parent">
       <calendar-view
+        :display-period-count="1"
         :time-format-options="{ hour: '2-digit', minute: '2-digit' }"
         :item-top="themeOptions.top"
         :item-content-height="themeOptions.height"
